2ed023fe22de8c49f92c5d8f49fdb9b70d1f28c4
Raciste Pro Max <43303508+BabaLeBob@users.noreply.github.com>
2025-12-19 17:53:43 +0100

    fsdf

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+fetch("cartes.json")
+  .then(response => response.json())
+  .then(cartes => {
+    const container = document.getElementById("cartes-container");
+
+    cartes.forEach(carte => {
+      const div = document.createElement("div");
+      div.className = "carte";
+
+      div.innerHTML = `
+        <h2>${carte.nom}</h2>
+        <p>Type : ${carte.type}</p>
+        <p>Attaque : ${carte.attaque}</p>
+        <img src="${carte.image}" alt="${carte.nom}">
+      `;
+
+      container.appendChild(div);
+    });
+  });
 
